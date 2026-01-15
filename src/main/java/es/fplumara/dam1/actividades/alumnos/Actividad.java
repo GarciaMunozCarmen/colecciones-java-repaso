33ae00ff430cfa65f416974d0683a0703e05ef1a
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-15 10:37:40 +0100

    Actividad Alumnos

--- src/main/java/es/fplumara/dam1/actividades/alumnos/Actividad.java
+++ src/main/java/es/fplumara/dam1/actividades/alumnos/Actividad.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class Actividad {
 
     public void procesar(List<Alumno> alumnosEntrada) {
-
+        Set<Alumno> alumnoSet = new TreeSet<>(alumnosEntrada); //Tree no los hace unicos ???? (por algun motivo desconocido)
+        System.out.printf("Total Alumnos: %d\nAlumnos no repetidos (%d):\n",alumnosEntrada.size(), alumnoSet.size());
+        alumnoSet.forEach(a-> System.out.println(a.toString()));
     }
 
 
--- src/main/java/es/fplumara/dam1/actividades/alumnos/Alumno.java
+++ src/main/java/es/fplumara/dam1/actividades/alumnos/Alumno.java
@@ -1,6 +1,8 @@
 package es.fplumara.dam1.actividades.alumnos;
 
-public class Alumno {
+import java.util.Objects;
+
+public class Alumno implements Comparable{
     private final String dni;
     private final String nombre;
 
@@ -23,4 +25,24 @@ public class Alumno {
         return "Alumno{dni='" + dni + "', nombre='" + nombre + "'}";
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Alumno alumno)) return false;
+        return Objects.equals(dni, alumno.dni);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dni);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Alumno a = (Alumno) o;
+        if(dni.equalsIgnoreCase(a.dni)) {
+            return nombre.compareTo(a.nombre);
+        }else {
+            return dni.compareTo(a.dni);
+        }
+    }
 }
