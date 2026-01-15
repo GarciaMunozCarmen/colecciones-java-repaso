5441dab1c81f305e760f11f981b3952c2ce1a051
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-15 09:42:08 +0100

    Actividad supermercado

--- src/main/java/es/fplumara/dam1/actividades/supermercado/Actividad.java
+++ src/main/java/es/fplumara/dam1/actividades/supermercado/Actividad.java
@@ -1,10 +1,22 @@
 package es.fplumara.dam1.actividades.supermercado;
 
-import java.util.List;
+import java.util.*;
 
 public class Actividad {
 
     public void procesarEscaneos(List<String> escaneos) {
+        escaneos = escaneos.stream().map(p -> p.trim().replace("  ", " ").toLowerCase()).toList();
+        Set<String> escaneosSet= new TreeSet<>();
+        for(String e:escaneos){
+            if(escaneosSet.add(e)) {
+                System.out.println("AÑADIDO: " + e);
+            }else {
+                System.out.println("REPETIDO: " + e);
+            }
+        }
+        System.out.printf("Total escaneos: %d\nTotal repetidos: %d\nTotal productos distintos: %d\n", escaneos.size(), (escaneos.size() - escaneosSet.size()), escaneosSet.size());
+        System.out.println("Listado final de productos:");
+        escaneosSet.forEach(escaneo-> System.out.println("  - " + escaneo));
 
     }
 
