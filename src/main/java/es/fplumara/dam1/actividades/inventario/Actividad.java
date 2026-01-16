ca1a1425ad36140ee071da07e57ce9bd68d3b120
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-16 14:12:36 +0100

    inicio almacen x_x

--- src/main/java/es/fplumara/dam1/actividades/inventario/Actividad.java
+++ src/main/java/es/fplumara/dam1/actividades/inventario/Actividad.java
@@ -1,10 +1,19 @@
 package es.fplumara.dam1.actividades.inventario;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Actividad {
 
     public void procesarMovimientos(List<String> movimientos) {
-
+        Map<String, Integer> movimientosMap = new TreeMap<>();
+        for (String movimiento : movimientos) {
+            String[] movimientoParts = movimiento.split(" ");
+            String elemento = movimientoParts[0];
+            Integer cantidad = Integer.parseInt(movimientoParts[1]);
+            Integer cantidadActual = movimientosMap.getOrDefault(elemento, 0);
+            movimientosMap.put(elemento, cantidad + cantidadActual);
+        }
     }
 }
