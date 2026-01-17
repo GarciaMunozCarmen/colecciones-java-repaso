c32ccf91967d956220bf7d5d787d45cc5014c945
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-17 20:59:37 +0100

    Actividad inventario terminada

--- src/main/java/es/fplumara/dam1/actividades/inventario/Actividad.java
+++ src/main/java/es/fplumara/dam1/actividades/inventario/Actividad.java
@@ -13,7 +13,26 @@ public class Actividad {
             String elemento = movimientoParts[0];
             Integer cantidad = Integer.parseInt(movimientoParts[1]);
             Integer cantidadActual = movimientosMap.getOrDefault(elemento, 0);
-            movimientosMap.put(elemento, cantidad + cantidadActual);
+            if ((cantidad + cantidadActual) >= 0){
+                movimientosMap.put(elemento, cantidad + cantidadActual);
+            }else{
+                System.out.printf("AVISO: stock insuficiente de %s\n", elemento);
+                movimientosMap.put(elemento, 0);
+            }
         }
+        for (Map.Entry<String, Integer> entry : movimientosMap.entrySet()) {
+            System.out.println(" - " + entry.getKey() + " -> " + entry.getValue());
+        }
+        int mayor = 0;
+        String masStock = "";
+        for (Map.Entry<String, Integer> entry : movimientosMap.entrySet()){
+            if (mayor > entry.getValue()){
+                continue;
+            }else{
+                mayor = entry.getValue();
+            }
+            masStock = entry.getKey();
+        }
+        System.out.printf("El producto con más stock es: %s", masStock);
     }
 }
